2b10f35fffc97795ce723a5602a00e5fd63ad73e
Marcell <marcell.dechant@neuefische.de>
2023-05-26 10:31:28 +0200

    BIG BANG

---
+++ src/main/java/de/neuefische/mucjava231cartask/MucJava231CarTaskApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.mucjava231cartask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MucJava231CarTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MucJava231CarTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/mucjava231cartask/MucJava231CarTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.mucjava231cartask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MucJava231CarTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
